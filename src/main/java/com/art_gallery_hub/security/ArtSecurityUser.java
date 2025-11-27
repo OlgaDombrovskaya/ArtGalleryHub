cbf7e65655aa79a974e205cb5ff683f828e2615d
DavidGrim777 <davidgrim676@gmail.com>
2025-11-27 13:17:01 +0100

    Add SecurityConfig +UserDetailsService (#5)

---
+++ src/main/java/com/art_gallery_hub/security/ArtSecurityUser.java
@@ -0,0 +1,40 @@
+package com.art_gallery_hub.security;
+
+import com.art_gallery_hub.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArtSecurityUser implements UserDetails {
+    private final User user;
+
+    public ArtSecurityUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
