815794f8ed87f16c09215c5b2cc6e31d9e2ce102
olgadombrovskaya <olgaekb26@gmail.com>
2025-11-28 11:40:57 +0000

    change application.properties (prod and dev)

--- src/main/java/com/art_gallery_hub/controller/PublicController.java
+++ src/main/java/com/art_gallery_hub/controller/PublicController.java
@@ -1,6 +1,27 @@
 package com.art_gallery_hub.controller;
 
 
+import com.art_gallery_hub.model.Artwork;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Tag(name = "Public Access / Authentication", description = "Login, registration," +
+        " and password recovery operations. Available without authentication")
+@RestController
+@RequestMapping("/api/public")
 public class PublicController {
+
+    @GetMapping("/info")
+    public String publicInfo() {
+        return "Welcome to Art Gallery Hub";
+    }
+
+@GetMapping("/artworks")
+    public List<Artwork> getArtworksByIsPublic() {
+
+}
 }
